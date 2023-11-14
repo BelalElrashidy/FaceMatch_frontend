3cc1cc95c217fc1481c75f8f92ce903c5fdafc82
BelalElrashidy <bebo.elrashidy22@gmail.com>
2023-11-14 19:39:47 +0300

    Uploading the remaining part

---
+++ src/components/Container.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="colored" v-if="color">
+    <div class="container">
+        <h1>{{ title }}</h1>
+        <hr>
+        <slot></slot>
+    </div>
+  </div>
+  <div class="white" v-else>
+    <div class="container">
+        <h1>{{ title }}</h1>
+        <hr>
+      <slot></slot>
+    </div>
+      
+  </div>
+</template>
+
+<script>
+export default {
+  name: "container",
+  props: {
+    title: String,
+    color: { require: false, type: Boolean, default: false },
+  },
+};
+</script>
+<style>
+@import "../assets/css/main.css";
+</style>
--- src/components/Pages/Projects.vue
+++ src/components/Pages/Projects.vue
@@ -1,5 +1,16 @@
 <template>
-    <h1>
-        Projects Page
-    </h1>
+   <div class="main">
+    <div style="width: 100vw; height: 100vh;">
+        <img src="../../assets/pictures/projects.jpg" alt="" style="width: 100vw;">
+    </div>
+    <container title="Our Projects">
+        
+    </container>
+</div>
 </template>
+<script>
+    import container from "../Container.vue";
+    export default {
+        components: {container}
+    }
+</script>
