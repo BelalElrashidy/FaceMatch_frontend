d4a1698a7baf9e4215e1bb567eb6f46c5adbde6f
BelalElrashidy <bebo.elrashidy22@gmail.com>
2023-11-14 19:59:55 +0300

    fixing bugs

--- src/Router/index.js
+++ src/Router/index.js
@@ -1,8 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../components/Pages/Home.vue';
-import about from '../components/pages/About.vue';
-import projects from '../components/pages/Projects.vue';
-import Notfound from '../components/pages/Notfound.vue';
+import about from '../components/Pages/About.vue';
+import projects from '../components/Pages/Projects.vue';
+import Notfound from '../components/Pages/Notfound.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
