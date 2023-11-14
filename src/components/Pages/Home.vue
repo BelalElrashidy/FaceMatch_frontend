a140c17ee307b1524a95af1f98bc442d2016c423
BelalElrashidy <bebo.elrashidy22@gmail.com>
2023-11-14 19:37:51 +0300

    REshaping the Home Screen

--- src/components/Pages/Home.vue
+++ src/components/Pages/Home.vue
@@ -1,20 +1,110 @@
 <template>
-    <img src="" alt="Main">
-    <div class="main">
-      
-      <container title="Goal" msg ="we aim to have the Best AI face recognition"/>
-      <container title="Aim" msg ="we aim to have the Best AI face recognition"/>
-      <container title="Purpose" msg ="we aim to have the Best AI face recognition"/>
-      <container title="Functionality" msg ="we aim to have the Best AI face recognition"/>
+    <div style="width: 100vw; height: 100vh;">
+
+        <img src="../../assets/pictures/cyborg-hand-3d-background-technology-artificial-intelligence.jpg" alt="Main" style="width: 100vw;" />
     </div>
+  <div class="main">
+    <container title="Goal" color>
+      <p>
+        Our primary goal is to establish ourselves as leaders in the field of
+        AI-driven applications and web development. We strive to demonstrate the
+        potential of AI in solving real-world problems and enhancing user
+        experiences. Through our portfolio and website, we aim to attract
+        clients, collaborators, and enthusiasts who share a passion for
+        innovative technology solutions. We aspire to contribute to the
+        advancement of AI and web development by showcasing our projects,
+        skills, and the practical applications of our expertise.
+      </p>
+    </container>
+    <container title="Aim">
+      <p>
+        Our aim is to provide a comprehensive view of our capabilities, with a
+        focus on the Facematch application as a standout example. We aim to
+        highlight the precision and efficiency of our AI algorithms, the
+        elegance of our web development solutions, and the seamless integration
+        of both to create a user-friendly experience. We seek to demonstrate how
+        our projects go beyond the theoretical aspects of technology, making a
+        tangible impact in the real world by solving problems and simplifying
+        complex tasks.
+      </p>
+    </container>
+    <container title="Purpose" color>
+      <p>
+        The purpose of our portfolio and website is to showcase our expertise in
+        the intersection of Artificial Intelligence, Web Development, and
+        innovative applications such as Facematch. We aim to demonstrate our
+        proficiency in leveraging cutting-edge technologies to create solutions
+        that seamlessly integrate AI capabilities with user-friendly web
+        interfaces. Our portfolio serves as a testament to our commitment to
+        pushing the boundaries of technology to deliver unique and impactful
+        applications.
+      </p>
+    </container>
+    <container title="Functionality">
+      <p>
+        The functionality of our portfolio and website revolves around three key
+        pillars:
+      </p>
+      <ol>
+        <li>
+          AI Showcase:
+          <ul>
+            <li>
+              Displaying a range of AI projects, including Facematch, to
+              illustrate our proficiency in developing and implementing advanced
+              algorithms.
+            </li>
+            <li>
+              Providing detailed insights into the AI methodologies employed,
+              highlighting accuracy, scalability, and adaptability.
+            </li>
+          </ul>
+        </li>
+        <li>
+          Web Development Excellence:
+          <ul>
+            <li>
+              Showcasing a portfolio of web development projects that emphasize
+              responsive design, intuitive user interfaces, and seamless
+              integration of backend functionalities.
+            </li>
+            <li>
+              Demonstrating our ability to create robust and scalable web
+              applications that complement the power of AI.
+            </li>
+          </ul>
+        </li>
+        <li>
+          Facematch Application Demo:
+          <ul>
+            <li>
+              Offering a live demonstration of the Facematch application,
+              allowing users to upload two pictures and witness the accurate
+              identification of faces common to both images. Providing technical
+              details on the underlying AI models, showcasing the application's
+              speed, accuracy, and versatility.
+            </li>
+          </ul>
+        </li>
+      </ol>
+      <p>
+        Through these functionalities, our portfolio and website aim to not only
+        inform but also engage visitors, encouraging them to explore the
+        possibilities of AI and web development in unison.
+      </p>
+    </container>
+  </div>
 </template>
 
 <script>
-    import container from "../text.vue";
+import container from "../Container.vue";
 
-    export default {
-        components: {
-            container
-        }
-    }
+export default {
+  components: {
+    container,
+  },
+};
 </script>
+<style>
+@import url(../../assets/css/main.css);
+</style>
