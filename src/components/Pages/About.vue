527bf31db9fb361dacf3db7985b3440eed5077f5
BelalElrashidy <bebo.elrashidy22@gmail.com>
2023-11-14 19:36:44 +0300

    About page

--- src/components/Pages/About.vue
+++ src/components/Pages/About.vue
@@ -1,26 +1,116 @@
 <template>
-    
-    <img src="" alt="About US">
-    <main>
-        <container title="Who are we?" msg="I am only human after all"/>
-    </main>
+    <div style="width: 100vw ; height: 100vh;">
+
+        <img src="../../assets/pictures/About us.jpg" alt="About US"  style="width: 100vw;"/>
+    </div>
+    <div class="main">
+    <container
+      title="About Us: Unleashing Innovation at the Intersection of AI and Web Development"
+    >
+      <p>
+        Welcome to [Your Company Name], where innovation knows no bounds.
+        Established with a passion for pushing technological frontiers, we are a
+        dynamic team of professionals dedicated to harnessing the power of
+        Artificial Intelligence and Web Development to create transformative
+        solutions.
+      </p>
+      <h2>Our Story:</h2>
+
+      <p>
+        At [Your Company Name], our journey began with a shared vision of
+        bringing together the realms of AI and web development to pioneer
+        groundbreaking applications. Fueled by a commitment to innovation and a
+        desire to make a meaningful impact, we embarked on a mission to redefine
+        the possibilities of technology.
+      </p>
+
+      <h2>What Sets Us Apart:</h2>
+      <ol>
+        <li>
+          Expertise in AI:
+          <p>
+            Our team comprises seasoned experts in the field of Artificial
+            Intelligence, with a deep understanding of machine learning,
+            computer vision, and neural networks. We leverage the latest
+            advancements in AI to craft solutions that go beyond theoretical
+            concepts, solving real-world challenges.
+          </p>
+        </li>
+        <li>
+          Mastery in Web Development:
+
+          <p>
+            Web development is our playground, where creativity meets
+            functionality. We pride ourselves on delivering web applications
+            that not only look visually stunning but also provide seamless user
+            experiences. Our developers are adept at utilizing the latest
+            frameworks and technologies to create robust and responsive
+            interfaces.
+          </p>
+        </li>
+        <li>
+          Innovative Applications:
+          <p>
+            - At the core of our identity is a commitment to innovation. Our
+            portfolio showcases a diverse range of projects, with a special
+            spotlight on our flagship application, Facematch. This pioneering
+            tool epitomizes our ability to merge AI and web development,
+            offering a glimpse into the future of technology.
+          </p>
+        </li>
+      </ol>
+
+      <h2>**Our Mission:**</h2>
+      <p>
+        We are on a mission to bridge the gap between human potential and
+        technological capabilities. Our endeavors are guided by the belief that
+        technology should not just exist but should enrich lives, simplify
+        processes, and open doors to new possibilities. Through a blend of AI
+        and web development, we aim to create solutions that are not only
+        intelligent but also accessible and user-friendly.
+      </p>
+      <h2>Why Choose [Your Company Name]:</h2>
+      <ul>
+        <li>
+          Innovation at the Core: We thrive on pushing boundaries and exploring
+          uncharted territories in technology.
+        </li>
+        <li>
+          Collaborative Approach: We believe in the power of collaboration,
+          working closely with clients to transform ideas into reality.
+        </li>
+        <li>
+          Commitment to Excellence: From concept to execution, we uphold the
+          highest standards of quality, ensuring that our solutions exceed
+          expectations.
+        </li>
+      </ul>
+
+      <h3>
+
+          Join us on this exciting journey where we transform ideas into reality,
+          reimagine possibilities, and redefine the future of technology. At [Your
+          Company Name], innovation is not just a word – it's a way of life.
+      </h3>
+    </container>
+</div>
 </template>
 <script>
-    import container from "../text.vue";
-    export default{
-        components:{
-            container
-        }
-    }
+import container from "../Container.vue";
+export default {
+  components: {
+    container,
+  },
+};
 </script>
-<style >
-    main{
-        display: flex;
-        flex-basis: 100%;
-        justify-content: center;
-        flex-direction: row;
-        align-items: center;
-        margin-bottom: 5rem ;
-        margin-top: 3rem ;
-    }
+<style>
+main {
+  display: flex;
+  flex-basis: 100%;
+  justify-content: center;
+  flex-direction: row;
+  align-items: center;
+  margin-bottom: 5rem;
+  margin-top: 3rem;
+}
 </style>
